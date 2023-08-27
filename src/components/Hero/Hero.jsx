9d7cb80f891c1b024e9307935556b8d055092e42
Vidushi <vidushirajput11@gmail.com>
2023-08-28 00:46:24 +0530

    commit

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -6,8 +6,8 @@ import hero_image_back from "../../assets/hero_image_back.png";
 import Heart from "../../assets/heart.png";
 import Calories from "../../assets/calories.png";
 
-const Hero = ()=>{
-    return(
+const Hero = () => {
+    return (
         <div className='hero'>
             <div className='left-h'>
                 <Header />
@@ -15,41 +15,41 @@ const Hero = ()=>{
                     <div></div>
                     <span>THE BEST FITNESS CLUB IN THE TOWN</span>
                 </div>
-            
 
-            <div className='hero-text'>
-                <div>
-                    <span className='stroke-text'>Shape </span>
-                    <span>Your</span>
+
+                <div className='hero-text'>
+                    <div>
+                        <span className='stroke-text'>Shape </span>
+                        <span>Your</span>
+                    </div>
+                    <div>
+                        <span>Ideal Body</span>
+                    </div>
+                    <div className='span'>
+                        <span> In here, we will help you to shape and build your ideal body and live up your life to fullest!</span>
+                    </div>
                 </div>
-                <div>
-                    <span>Ideal Body</span>
+
+                <div className="figures">
+                    <div>
+                        <span>+140 </span>
+                        <span>EXPERT COACHES</span>
+                    </div>
+                    <div>
+                        <span>+978 </span>
+                        <span>MEMBERS JOINED</span>
+                    </div>
+                    <div>
+                        <span>+50 </span>
+                        <span>FITNESS PROGRAMS</span>
+                    </div>
                 </div>
-                <div className='span'>
-                   <span> In here, we will help you to shape and build your ideal body and live up your life to fullest!</span>
+
+                <div className='hero-buttons'>
+                    <button className="btn">Get started</button>
+                    <button className="btn">Learn more</button>
                 </div>
             </div>
-
-            <div className="figures">
-                <div>
-                    <span>+140 </span>
-                    <span>EXPERT COACHES</span>
-                </div>
-                <div>
-                    <span>+978 </span>
-                    <span>MEMBERS JOINED</span>
-                </div>
-                <div>
-                    <span>+50 </span>
-                    <span>FITNESS PROGRAMS</span>
-                </div>
-            </div>
-
-            <div className='hero-buttons'>
-                <buttons className="btn">Get started</buttons>
-                <buttons className="btn">Learn more</buttons>
-            </div>
-        </div>
             <div className='right-h'>
                 <button className='btn'>Join Now</button>
                 <div className='heart-rate'>
@@ -60,6 +60,14 @@ const Hero = ()=>{
 
                 <img src={hero_image} alt='' className='hero-image' />
                 <img src={hero_image_back} alt='' className='hero-image_back' />
+
+                <div className="calories">
+                    <img src={Calories} alt="" />
+                    <div>
+                        <span>Calories burned </span>
+                        <span>220 Kcal</span>
+                    </div>
+                </div>
             </div>
         </div>
     )
