553d9d427b7f9f5fc64c523e4562b62608ebe48b
Vidushi <vidushirajput11@gmail.com>
2023-08-29 13:01:16 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import './App.css';
+import { Footer } from './components/Footer/Footer';
 import Hero from './components/Hero/Hero';
+import { Join } from './components/Join/Join';
 import { Plans } from './components/Plans/Plans';
 import Programs from './components/Programs/Programs';
 import { Reasons } from './components/Reasons/Reasons';
 import { Testimonials } from './components/Testimonials/Testimonials';
+
 
 function App() {
   return(
@@ -13,6 +16,8 @@ function App() {
       <Reasons />
       <Plans />
       <Testimonials />
+      <Join />
+      <Footer />
     </div>
   )
 }
